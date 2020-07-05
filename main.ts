c0209bd827d3a0f540c713632e9587f59d315632
rtator <66912559+rtator@users.noreply.github.com>
2020-07-04 17:54:30 -0700

    smash-bros.-like game

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+namespace SpriteKind {
+    export const fist = SpriteKind.create()
+    export const pl_1 = SpriteKind.create()
+    export const pl_2 = SpriteKind.create()
+    export const fist2 = SpriteKind.create()
+}
 namespace myTiles {
     //% blockIdentity=images._tile
     export const tile0 = img`
@@ -19,27 +25,64 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 `
 }
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Released, function () {
+    mySprite3.destroy()
+})
 controller.player1.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
     ispl1facingleft = false
 })
-controller.player2.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Released, function () {
-	
+sprites.onOverlap(SpriteKind.fist, SpriteKind.pl_2, function (sprite, otherSprite) {
+    info.player1.changeScoreBy(1)
 })
-controller.player1.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Released, function () {
-	
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Released, function () {
+    mySprite3.destroy()
 })
 controller.player1.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
     if (mySprite.vy == 0) {
-        mySprite.vy = -110
+        mySprite.vy = -120
     }
 })
-controller.player1.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
+sprites.onOverlap(SpriteKind.fist2, SpriteKind.pl_1, function (sprite, otherSprite) {
+    info.player2.changeScoreBy(1)
+})
+controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
+    if (ispl2facingleft) {
+        mySprite3 = sprites.create(img`
+3 3 3 . . 
+3 3 3 3 3 
+3 3 3 . . 
+`, SpriteKind.fist2)
+        mySprite3.setPosition(mySprite2.x, mySprite2.y)
+        mySprite3.setVelocity(-100, 0)
+        pause(50)
+        mySprite3.setVelocity(0, 0)
+    } else if (!(ispl2facingleft)) {
+        mySprite3 = sprites.create(img`
+. . 3 3 3 
+3 3 3 3 3 
+. . 3 3 3 
+`, SpriteKind.fist2)
+        mySprite3.setPosition(mySprite2.x, mySprite2.y)
+        mySprite3.setVelocity(100, 0)
+        pause(50)
+        mySprite3.setVelocity(0, 0)
+    }
+})
+controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
+    ispl2facingleft = true
+})
+controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
+    if (mySprite2.vy == 0) {
+        mySprite2.vy = -120
+    }
+})
+controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
     if (ispl1facingleft) {
         mySprite3 = sprites.create(img`
 2 2 2 . . 
 2 2 2 2 2 
 2 2 2 . . 
-`, SpriteKind.Player)
+`, SpriteKind.fist)
         mySprite3.setPosition(mySprite.x, mySprite.y)
         mySprite3.setVelocity(-100, 0)
         pause(50)
@@ -56,45 +99,16 @@ controller.player1.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Press
         mySprite3.setVelocity(0, 0)
     }
 })
-controller.player2.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
-    if (mySprite2.vy == 0) {
-        mySprite2.vy = -110
-    }
-})
-controller.player2.onButtonEvent(ControllerButton.Left, ControllerButtonEvent.Pressed, function () {
-    ispl2facingleft = true
-})
-controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
-    if (mySprite2.vy == 0) {
-        mySprite2.vy = -110
-    }
-})
-controller.player2.onButtonEvent(ControllerButton.B, ControllerButtonEvent.Pressed, function () {
-    if (ispl2facingleft) {
-        mySprite3 = sprites.create(img`
-3 3 3 . . 
-3 3 3 3 3 
-3 3 3 . . 
-`, SpriteKind.Player)
-        mySprite3.setPosition(mySprite2.x, mySprite2.y)
-        mySprite3.setVelocity(-100, 0)
-        pause(50)
-        mySprite3.setVelocity(0, 0)
-    } else if (!(ispl2facingleft)) {
-        mySprite3 = sprites.create(img`
-. . 3 3 3 
-3 3 3 3 3 
-. . 3 3 3 
-`, SpriteKind.Player)
-        mySprite3.setPosition(mySprite2.x, mySprite2.y)
-        mySprite3.setVelocity(100, 0)
-        pause(50)
-        mySprite3.setVelocity(0, 0)
-    }
-})
-controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function () {
-    if (mySprite.vy == 0) {
-        mySprite.vy = -110
+info.onCountdownEnd(function () {
+    if (info.player2.score() < info.player1.score()) {
+        game.showLongText("player1 wins!", DialogLayout.Bottom)
+        game.over(true)
+    } else if (info.player2.score() == info.player1.score()) {
+        game.showLongText("tie!", DialogLayout.Bottom)
+        game.over(false)
+    } else {
+        game.showLongText("player wins!", DialogLayout.Bottom)
+        game.over(true)
     }
 })
 controller.player2.onButtonEvent(ControllerButton.Right, ControllerButtonEvent.Pressed, function () {
@@ -117,8 +131,8 @@ mySprite = sprites.create(img`
 2 2 2 2 2 2 2 2 
 2 2 2 2 2 2 2 2 
 2 2 2 2 2 2 2 2 
-`, SpriteKind.Player)
-mySprite.setPosition(130, 27)
+`, SpriteKind.pl_1)
+mySprite.setPosition(150, 27)
 controller.player1.moveSprite(mySprite, 100, 0)
 mySprite2 = sprites.create(img`
 3 3 3 3 3 3 3 3 
@@ -129,17 +143,17 @@ mySprite2 = sprites.create(img`
 3 3 3 3 3 3 3 3 
 3 3 3 3 3 3 3 3 
 3 3 3 3 3 3 3 3 
-`, SpriteKind.Player)
-mySprite2.setPosition(30, 27)
+`, SpriteKind.pl_2)
+mySprite2.setPosition(10, 27)
 controller.player2.moveSprite(mySprite2, 100, 0)
 tiles.setTilemap(tiles.createTilemap(
-            hex`0a0007000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000d0c0c0c0c0c0c0c0c0e`,
+            hex`0a0007000000000000000000000000000000000000000000000000000d0e0000000000000000000000000000000d0c0e00000d0c0e00000000000000000000000d0c0c0c0c0c0c0c0c0e`,
             img`
 . . . . . . . . . . 
 . . . . . . . . . . 
+. . . . 2 2 . . . . 
 . . . . . . . . . . 
-. . . . . . . . . . 
-. . . . . . . . . . 
+. 2 2 2 . . 2 2 2 . 
 . . . . . . . . . . 
 2 2 2 2 2 2 2 2 2 2 
 `,
@@ -148,5 +162,6 @@ tiles.setTilemap(tiles.createTilemap(
         ))
 mySprite.ay = 200
 mySprite2.ay = 200
+info.startCountdown(60)
 ispl2facingleft = true
 ispl1facingleft = false
